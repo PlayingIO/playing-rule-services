575128d9081e4e8c72b7cb95881d846ef56e0201
forafish <fora.luo@gmail.com>
2018-02-14 02:36:44 +0800

    update reward schema

--- src/models/reward-schema.js
+++ src/models/reward-schema.js
@@ -1,13 +1,12 @@
 // reward structure
 const reward = {
-  metric: {                                // the metric which will be used for the reward
-    id: { type: 'ObjectId' },              // ID of the metric
-    type: { type: String },                // type of the metric
-  },
-  probabilty: { type: Number },            // chance [0, 1] that this reward in an action or process task can be given
+  metric: { type: 'ObjectId' },            // the metric which will be used for the reward
+  type: { type: String },                  // type of the metric
   verb: { type: String, enum: [            // operation is performed for this reward
     'add', 'remove', 'set'
   ]},
+  item: { type: String },                  // name of the set metric
+  probabilty: { type: Number },            // chance [0, 1] that this reward in an action or process task can be given
   value: { type: String },                 // value by which the player's score changes
   recur: { type: Boolean },                // apply the reward on every loop of the looped task
   resolution: { type: String },            // delay the rewards until the resolution task
