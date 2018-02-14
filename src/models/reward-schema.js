6142d2af5eca99ea9bc65b87828c656c74acd845
forafish <fora.luo@gmail.com>
2018-02-14 21:29:59 +0800

    rename probability to chance in reward schema

--- src/models/reward-schema.js
+++ src/models/reward-schema.js
@@ -1,15 +1,15 @@
 // reward structure
 const reward = {
-  metric: { type: 'ObjectId' },            // the metric which will be used for the reward
-  type: { type: String },                  // type of the metric
-  verb: { type: String, enum: [            // operation is performed for this reward
+  metric: { type: 'ObjectId' },               // the metric which will be used for the reward
+  type: { type: String },                     // type of the metric
+  verb: { type: String, enum: [               // operation is performed for this reward
     'add', 'remove', 'set'
   ]},
-  item: { type: String },                  // name of the set metric
-  probabilty: { type: Number },            // chance [0, 1] that this reward in an action or process task can be given
-  value: { type: String },                 // value by which the player's score changes
-  recur: { type: Boolean },                // apply the reward on every loop of the looped task
-  resolution: { type: String },            // delay the rewards until the resolution task
+  item: { type: String },                     // name of the set metric
+  chance: { type: Number, default: 100 },     // chance [0, 100] that this reward in an action or process task can be given
+  value: { type: String },                    // value by which the player's score changes
+  recur: { type: Boolean },                   // apply the reward on every loop of the looped task
+  resolution: { type: String },               // delay the rewards until the resolution task
 };
 
 export default { reward }
