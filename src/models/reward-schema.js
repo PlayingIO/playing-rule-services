ffe9f6ea10ac9c51a983c18cee588f99d6718fbd
forafish <fora.luo@gmail.com>
2018-02-25 23:16:42 +0800

    Bump version

--- src/models/reward-schema.js
+++ src/models/reward-schema.js
@@ -8,7 +8,7 @@ const reward = {
   item: { type: String },                     // name of the set metric
   chance: { type: Number, default: 100 },     // chance [0, 100] that this reward in an action or process task can be given
   value: { type: String },                    // value by which the player's score changes
-  recur: { type: Boolean },                   // wether apply the reward on every loop of the looped task
+  recur: { type: Boolean },                   // whether apply the reward on every loop of the looped task
                                               //   or only after all it's loops are complete
   resolution: { type: String },               // delay the rewards until the resolution task
 };
