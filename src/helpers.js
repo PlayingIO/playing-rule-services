8a5125ca7a588a4b587619da7464957d368a4da9
forafish <fora.luo@gmail.com>
2018-02-28 17:27:23 +0800

    add fulfillRequires skeleton logic

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,66 @@
 import fp from 'mostly-func';
 
-export const fulfillRequires = (cond) => {
+const fulfillMetric = (cond) => {
   return true;
 };
 
-export const fulfillRewards = (rules) => {
+const fulfillAction = (cond) => {
+  return true;
+};
+
+const fulfillTeam = (cond) => {
+  return true;
+};
+
+const fulfillTime = (cond) => {
+  return true;
+};
+
+const fulfillFormula = (cond) => {
+  return true;
+};
+
+export const fulfillRequires = (conditions) => {
+  if (!conditions) return true; // requires no conditions
+  return fp.all(cond => {
+    if (!cond) return true;
+    switch (cond.rule) {
+      case 'metric': return fulfillMetric(cond);
+      case 'action': return fulfillAction(cond);
+      case 'team': return fulfillTeam(cond);
+      case 'time': return fulfillTime(cond);
+      case 'formula': return fulfillFormula(cond);
+      case 'and': return cond.conditions && fp.all(fulfillRequires(cond.conditions));
+      case 'or': return cond.conditions && fp.any(fulfillRequires(cond.conditions));
+      default:
+        console.warn('fulfillRequires condition rule not supported', cond.rule);
+        return true;
+    }
+  }, conditions);
+};
+
+export const fulfillAchievementRewards = (achievement) => {
+  if (achievement.rules) {
+    return fp.reduce((arr, rule) => {
+      if (rule.item && rule.item.name && rule.item.number) {
+        if (fulfillRequires(rule.requires)) {
+          const reward = {
+            metric: achievement.metric,
+            item: rule.item.name,
+            verb: 'add',
+            value: rule.item.number
+          };
+          return fp.concat(arr, [reward]);
+        }
+      } else {
+        console.warn('process achievement rule skipped:', rule);
+      }
+      return arr;
+    }, [], achievement.rules);
+  }
+};
+
+export const fulfillCustomRewards = (rules) => {
   // filter by the rule requirements
   const activeRules = fp.filter(rule => {
     return fp.all(fulfillRequires, rule.requires);
