03efa34c7af41582ceb87128a18e9120eb012013
forafish <fora.luo@gmail.com>
2018-02-28 17:27:57 +0800

    basic logic for processing achievement rule

--- src/services/user-rule/user-rule-service.js
+++ src/services/user-rule/user-rule-service.js
@@ -3,6 +3,7 @@ import makeDebug from 'debug';
 import { Service as BaseService } from 'mostly-feathers';
 import fp from 'mostly-func';
 import defaultHooks from './user-rule-hooks';
+import { fulfillAchievementRewards } from '../../helpers';
 
 const debug = makeDebug('playing:user-rules-services:user-rules');
 
@@ -29,6 +30,8 @@ class UserRuleService extends BaseService {
     assert(data.user, 'params.user not provided');
 
     const svcRules = this.app.service('rules');
+    const svcUserMetrics = this.app.service('user-metrics');
+
     // get available rules
     const getRules = () => svcRules.find({
       query: { $select: [
@@ -43,8 +46,45 @@ class UserRuleService extends BaseService {
       paginate: false
     });
 
+    const createRewards = fp.reduce((arr, reward) => {
+      if (reward.metric) {
+        reward.metric = reward.metric.id || reward.metric;
+        reward.user = data.user;
+        arr.push(svcUserMetrics.create(reward));
+      }
+      return arr;
+    }, []);
+
+    const processAchievement = (achievement, variables) => {
+      if (achievement.metric) {
+        assert(achievement.metric.type, 'metric of achievement rule has not populated for process');
+        assert(achievement.metric.type === 'set', 'metric of achievement rule must be a set metric');
+        if (achievement.rules) {
+          const rewards = fulfillAchievementRewards(achievement);
+          if (rewards.length > 0) {
+            return Promise.all(createRewards(rewards));
+          }
+        }
+      }
+      return Promise.resolve(null);
+    };
+
+    const processLevel = (achievement, variables) => {
+      return Promise.resolve(null);
+    };
+
+    const processCustom = (achievement, variables) => {
+      return Promise.resolve(null);
+    };
+
     return getRules().then(results => {
-      return results;
+      return Promise.all(fp.map(rule => {
+        switch (rule.type) {
+          case 'achievement': return processAchievement(rule.achievement, rule.variables);
+          case 'level': return processLevel(rule.level, rule.variables);
+          case 'custom': return processCustom(rule.custom, rule.variables);
+        }
+      }, results));
     });
   }
 }
