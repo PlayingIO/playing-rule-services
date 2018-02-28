9dd8522515b49658512dd4725b2afae3dfb53595
forafish <fora.luo@gmail.com>
2018-02-28 14:53:57 +0800

    using service cacheMap

--- src/services/rule/rule-hooks.js
+++ src/services/rule/rule-hooks.js
@@ -2,12 +2,16 @@ import { hooks as auth } from 'feathers-authentication';
 import { hooks } from 'mostly-feathers-mongoose';
 import RuleEntity from '~/entities/rule-entity';
 import populateRequires from '../../hooks/populate-requires';
+import LRU from 'lru-cache';
+
+const cacheMap = new LRU({ max: 100 });
 
 module.exports = function(options = {}) {
   return {
     before: {
       all: [
-        auth.authenticate('jwt')
+        auth.authenticate('jwt'),
+        hooks.cache(cacheMap)
       ],
       get: [],
       find: [],
@@ -18,6 +22,7 @@ module.exports = function(options = {}) {
     },
     after: {
       all: [
+        hooks.cache(cacheMap),
         hooks.populate('achievement.metric', { service: 'sets' }),
         hooks.populate('level.state', { service: 'states' }),
         hooks.populate('level.point', { service: 'points' }),
