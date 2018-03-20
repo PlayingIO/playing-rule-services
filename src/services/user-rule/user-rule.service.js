00446866518a571fc2e0fc1952ba08845a018599
forafish <fora.luo@gmail.com>
2018-03-20 17:54:25 +0800

    refactoring with async/await, fix process with variables

--- src/services/user-rule/user-rule.service.js
+++ src/services/user-rule/user-rule.service.js
@@ -3,8 +3,15 @@ import makeDebug from 'debug';
 import { Service as BaseService } from 'mostly-feathers';
 import { helpers } from 'mostly-feathers-mongoose';
 import fp from 'mostly-func';
+import { helpers as metrics } from 'playing-metric-services';
+
 import defaultHooks from './user-rule.hooks';
-import { fulfillAchievementRewards, fulfillLevelRewards, fulfillCustomRewards } from '../../helpers';
+import {
+  getAllVariables,
+  fulfillAchievementRewards,
+  fulfillLevelRewards,
+  fulfillCustomRewards
+} from '../../helpers';
 
 const debug = makeDebug('playing:user-rules-services:user-rules');
 
@@ -26,9 +33,10 @@ class UserRuleService extends BaseService {
   /**
    * rules process for current player
    */
-  create(data, params) {
+  async create(data, params) {
     assert(data.user, 'data.user not provided');
     assert(params.user && params.user.scores, 'params.user.scores not provided');
+    data.variables = data.variables || {};
 
     const svcRules = this.app.service('rules');
     const svcUserMetrics = this.app.service('user-metrics');
@@ -45,61 +53,52 @@ class UserRuleService extends BaseService {
       paginate: false
     });
 
-    // create rewards
-    const createRewards = fp.reduce((arr, reward) => {
-      if (reward.metric) {
-        reward.metric = helpers.getId(reward.metric);
-        reward.user = data.user;
-        arr.push(svcUserMetrics.create(reward));
-      }
-      return arr;
-    }, []);
-
     // process achivement rule
-    const processAchievement = (achievement, variables) => {
+    const processAchievement = async (achievement, variables) => {
       if (achievement.metric) {
         assert(achievement.metric.type, 'metric of achievement rule has not populated for process');
         assert(achievement.metric.type === 'set', 'metric of achievement rule must be a set metric');
         if (achievement.rules) {
           const rewards = fulfillAchievementRewards(achievement, params.user);
-          return Promise.all(createRewards(rewards || []));
+          return metrics.createUserMetrics(this.app)(data.user, rewards || [], variables);
         }
       }
-      return Promise.resolve(null);
+      return [];
     };
 
     // process level rule
-    const processLevel = (level, variables) => {
+    const processLevel = async (level, variables) => {
       if (level.state && level.point) {
         assert(level.state.type && level.point.type, 'state or point of level rule has not populated for process');
         assert(level.state.type === 'state', 'state of level rule must be a state metric');
         assert(level.point.type === 'point', 'point of level rule must be a point metric');
         if (level.levels) {
           const rewards = fulfillLevelRewards(level, params.user);
-          return Promise.all(createRewards(rewards || []));
+          return metrics.createUserMetrics(this.app)(data.user, rewards || [], variables);
         }
       }
-      return Promise.resolve(null);
+      return [];
     };
 
     // process custom rule
-    const processCustom = (custom, variables) => {
+    const processCustom = async (custom, variables) => {
       if (custom.rules) {
         const rewards = fulfillCustomRewards(custom.rules, variables, params.user);
-        return Promise.all(createRewards(rewards || []));
+        return metrics.createUserMetrics(this.app)(data.user, rewards || [], variables);
       }
-      return Promise.resolve(null);
+      return [];
     };
 
-    return getRules().then(results => {
-      return Promise.all(fp.map(rule => {
-        switch (rule.type) {
-          case 'achievement': return processAchievement(rule.achievement, rule.variables);
-          case 'level': return processLevel(rule.level, rule.variables);
-          case 'custom': return processCustom(rule.custom, rule.variables);
-        }
-      }, results)).then(fp.flatten);
-    });
+    const rules = await getRules();
+    const results = await Promise.all(fp.flatMap(rule => {
+      const variables = getAllVariables(data.variables, rule.variables);
+      switch (rule.type) {
+        case 'achievement': return processAchievement(rule.achievement, variables);
+        case 'level': return processLevel(rule.level, variables);
+        case 'custom': return processCustom(rule.custom, variables);
+      }
+    }, rules));
+    return results;
   }
 }
 
