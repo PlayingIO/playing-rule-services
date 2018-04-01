a95a6b641ccab7b5e66479c060c5ef6338f285b8
forafish <fora.luo@gmail.com>
2018-04-02 03:10:07 +0800

    add time log for esm loader

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require = require("esm")(module/*, options*/);
+console.time('playing-rule-services import');
 module.exports = require('./src/index').default;
 module.exports.entities = require('./src/entities').default;
 module.exports.models = require('./src/models').default;
 module.exports.hooks = require('./src/hooks');
 module.exports.helpers = require('./src/helpers');
+console.time('playing-rule-services import');
