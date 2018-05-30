c7484d7f65dd494d651b6c2798f1475394d24745
forafish <fora.luo@gmail.com>
2018-05-30 17:03:52 +0800

    use playing-content-common

--- src/entities/rule.entity.js
+++ src/entities/rule.entity.js
@@ -1,8 +1,8 @@
 import Entity from 'mostly-entity';
-import { entities as contents } from 'playing-content-services';
+import { BlobEntity } from 'playing-content-common';
 
 const RuleEntity = new Entity('Rule', {
-  image: { using: contents.BlobEntity }
+  image: { using: BlobEntity }
 });
 
 RuleEntity.excepts('updatedAt', 'destroyedAt');
