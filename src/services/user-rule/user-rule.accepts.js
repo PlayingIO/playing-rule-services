687a7e33ec36e8616f97acc7075cfec334e57e1a
forafish <fora.luo@gmail.com>
2018-07-19 16:28:49 +0800

    lint empty lines

--- src/services/user-rule/user-rule.accepts.js
+++ src/services/user-rule/user-rule.accepts.js
@@ -5,7 +5,7 @@ module.exports = function accepts (context) {
   // validation rules
   const user = { arg: 'user', type: 'string', required: true, description: 'Current user' };
   const variables = { arg: 'variables', type: 'object', default: {}, description: 'Variables of metric' };
-  
+
   return {
     create: [ user, variables ]
   };
